63e9263cd4352f11ff93043e4f5999b197745fc4
brahime1234 <brahimek6375@yahoo.com>
2026-07-12 17:14:02 +0200

    UI: Add Web Empire agency credit logo and phone number to footer

--- obj/Debug/net8.0/DrKchida.AssemblyInfo.cs
+++ obj/Debug/net8.0/DrKchida.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrKchida")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df007a6afa07a521df0b9ba2f886631d385d8660")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df479af30351855928eb3d6f00e1354213bde6fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrKchida")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrKchida")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
